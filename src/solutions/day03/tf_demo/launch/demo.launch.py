45bd520daabfba3406a4b32aabc65da3d37cd613
ThomasBirchler <tom.birchler@hotmail.com>
2025-06-30 10:34:10 -0400

    moved colcon ignore and added launch settings for rviz

--- src/solutions/day03/tf_demo/launch/demo.launch.py
+++ src/solutions/day03/tf_demo/launch/demo.launch.py
@@ -1,8 +1,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    rviz_config_path = os.path.join(
+        get_package_share_directory('tf_demo'),
+        'rviz',
+        'tf_view.rviz'
+    )
+    
     return LaunchDescription([
         Node(
             package='tf_demo',
@@ -17,7 +25,7 @@ def generate_launch_description():
         Node(
             package='rviz2',
             executable='rviz2',
-            arguments=['-d', 'rviz/tf_view.rviz'],
+            arguments=['-d', rviz_config_path],
             output='screen'
         )
     ])
